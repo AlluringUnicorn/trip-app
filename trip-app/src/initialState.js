b5b4a54ed1c790a32a87d812639ee0e1d62870ee
Mariia Khapun <111394270+AlluringUnicorn@users.noreply.github.com>
2023-08-09 14:26:46 +0300

    Update initialState.js

--- trip-app/src/initialState.js
+++ trip-app/src/initialState.js
@@ -3,14 +3,14 @@
     id: 1,
     city: "London",
     image: "/cities-images/london.jpeg",
-    startDate: "2023-08-14",
-    endDate: "2023-08-21",
+    startDate: "2023-08-11",
+    endDate: "2023-08-15",
   },
   {
     id: 2,
     city: "Barcelona",
     image: "/cities-images/Barcelona.jpg",
-    startDate: "2023-08-26",
+    startDate: "2023-08-17",
     endDate: "2023-08-30",
   },
  ];
